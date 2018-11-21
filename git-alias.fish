04368f7804e961493a3f07827d02c5c6bf7d171f
Eliot Baker <eliotbaker@gmail.com>
2018-11-20 19:11:22 -0800

    fish git alias

---
+++ git-alias.fish
@@ -0,0 +1,33 @@
+# Git related
+# alias gc 'git commit -v' # zsh git plugin definition
+unalias gc
+alias gs 'git status'
+alias gc 'git commit -m'
+alias gcam 'git commit -a -m' # zsh git plugin definition
+alias gcv 'git commit -v'
+alias ga 'git add' # zsh git plugin
+alias gd 'git diff' # zsh git plugin
+alias gb 'git branch' # zsh git plugin
+# alias gl 'git pull' # zsh git plugin
+# alias gp 'git push' # zsh git plugin
+unalias gl
+alias gl 'git log'
+alias gu 'git pull'
+# alias gsb 'git status -sb'
+unalias gsb
+alias gsb 'git show-branch'
+# alias gco 'git checkout' # zsh git plugin definition
+unalias gco
+alias go 'git checkout'
+alias gg 'git grep'
+alias gk 'gitk --all'
+alias gr 'git rebase'
+alias gri 'git rebase --interactive'
+alias gcp 'git cherry-pick'
+alias grm 'git rm'
+alias gt 'git stash'
+alias gcv 'git commit --no-verify'
+alias gcn 'git commit --no-verify'
+
+# git mispelled
+alias got 'git '
