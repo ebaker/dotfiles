93bda8b8b0aadf3f747858dd57fbb6ea73bba720
Eliot Baker <eliot.baker@yara.com>
2019-12-02 12:58:53 -0800

    install nvm comment out

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 DOTFILES_DIR=$(pwd)
 
 source $DOTFILES_DIR/setup/1_setup_vim.sh
-source $DOTFILES_DIR/setup/2_setup_nvm.sh
+# source $DOTFILES_DIR/setup/2_setup_nvm.sh
 source $DOTFILES_DIR/setup/3_setup_zsh_zim.sh
