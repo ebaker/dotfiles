6f6225c99f2d09b3a3381b4f3c2d42cdfa708a09
Eliot Baker <eliot.baker@yara.com>
2019-12-04 21:13:38 -0800

    Moved dashboard config to elisp/eliot-dashboard.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -780,32 +780,8 @@ With prefix ARG, silently save all file-visiting buffers, then kill."
   ;; (add-to-list 'lsp-language-id-configuration '(rjsx-mode . "javascript"))
   )
 
-;; ;; Dashboard
-;; (use-package dashboard
-;;   :ensure t
-;;   :config
-;;   (dashboard-setup-startup-hook))
-
-;; ;; Set the title
-;; (setq dashboard-banner-logo-title "Welcome to Emacs Dashboard")
-;; ;; Set the banner
-;; (setq dashboard-startup-banner 3)
-;; ;; Value can be
-;; ;; 'official which displays the official emacs logo
-;; ;; 'logo which displays an alternative emacs logo
-;; ;; 1, 2 or 3 which displays one of the text banners
-;; ;; "path/to/your/image.png" which displays whatever image you would prefer
-
-;; ;; Content is not centered by default. To center, set
-;; (setq dashboard-center-content t)
-
-;; ;; To disable shortcut "jump" indicators for each section, set
-;; (setq dashboard-show-shortcuts nil)
-
-;; ;; go to dashboard on startup
-
-;; (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
-
+;; Dashboard
+;; (require 'eliot-dashboard)
 
 
 (custom-set-faces
