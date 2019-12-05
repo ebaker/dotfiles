f35d941dc1217a540c9c2cee74eab4e84bad5530
Eliot Baker <eliot.baker@yara.com>
2019-12-04 21:16:17 -0800

    Moved lsp config to elisp/eliot-lsp & commented out lsp.

---
+++ .emacs.d/elisp/eliot-lsp.el
@@ -0,0 +1,41 @@
+;;
+;; LSP
+;;
+;; lsp-mode
+(use-package lsp-mode
+  :ensure t
+  :init
+  (setq lsp-prefer-flymake t)
+  ;; (setq lsp-session-file (concat doom-etc-dir "lsp-session"))
+
+  ;; Don't prompt the user for the project root every time we open a new
+  ;; lsp-worthy file, instead, try to guess it with projectile.
+  (setq lsp-auto-guess-root t)
+
+  ;; Auto-kill LSP server once you've killed the last buffer associated with its
+  ;; project.
+  (setq lsp-keep-workspace-alive nil)
+  :config
+  (add-hook 'prog-major-mode #'lsp-prog-major-mode-enable)
+  (custom-set-variables '(lsp-eldoc-hook nil))
+  )
+
+;; lsp-ui
+(use-package lsp-ui
+  :ensure t
+  :init
+  (setq	lsp-ui-flycheck-enable nil
+	;; lsp-ui-sideline-enable nil
+	;; lsp-ui-doc-enable nil
+	;; lsp-ui-imenu-enable nil
+	;; ;; lsp-ui-sideline-ignore-duplicate t
+	;; ;;	lsp-ui-doc-max-height 8
+	;; ;;	lsp-ui-doc-max-width 35
+		)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+  ;; (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  ;; (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+  )
+
+(provide 'eliot-lsp)
