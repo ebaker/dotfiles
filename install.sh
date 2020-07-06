d5ce5bf1310cf7dded8af40a8a64f46ba4d07c4b
Eliot Baker <430383+ebaker@users.noreply.github.com>
2020-07-06 00:14:02 -0700

    add brew packages from txt and use in install script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 DOTFILES_DIR=$(pwd)
 
+source $DOTFILES_DIR/setup/0_brew_install.sh
 source $DOTFILES_DIR/setup/1_setup_vim.sh
 # source $DOTFILES_DIR/setup/2_setup_nvm.sh
 source $DOTFILES_DIR/setup/3_setup_zsh_zim.sh
