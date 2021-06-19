29042204585ce31ac9f757d640b99eb6aac715f1
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-06-19 13:05:51 -0700

    (emacs) magit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -452,6 +452,28 @@ With prefix ARG, silently save all file-visiting buffers, then kill."
   ;; rainbow delimiters
   (use-package rainbow-delimiters
     :hook (prog-mode . rainbow-delimiters-mode))
+;; Git
+(use-package magit
+  :bind ("C-M-;" . magit-status)
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(ebaker/leader-keys
+  "g"   '(:ignore t :which-key "git")
+  "gs"  'magit-status
+  "gd"  'magit-diff-unstaged
+  "go"  'magit-branch-or-checkout
+  "gl"   '(:ignore t :which-key "log")
+  "glc" 'magit-log-current
+  "glf" 'magit-log-buffer-file
+  "gb"  'magit-branch
+  "gP"  'magit-push-current
+  "gp"  'magit-pull-branch
+  "gf"  'magit-fetch
+  "gF"  'magit-fetch-all
+  "gr"  'magit-rebase)
+
 ;;
 ;; Flyspell
 ;;
