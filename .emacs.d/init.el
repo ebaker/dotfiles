7faf358a8cb157cd64109cfd9d0651427f05ccd1
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-06-19 11:06:26 -0700

    (emacs) dired

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -681,6 +681,28 @@ When using Homebrew, install it using \"brew install trash\"."
   (call-process (executable-find "trash")
     nil 0 nil
     file)))
+
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :bind (("C-x C-j" . dired-jump))
+  :custom ((dired-listing-switches "-aFl"))
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "h" 'dired-single-up-directory
+    "l" 'dired-single-buffer))
+
+(use-package dired-single)
+
+;; (use-package all-the-icons-dired
+;; :hook (dired-mode . all-the-icons-dired-mode))
+
+(use-package dired-hide-dotfiles
+  :hook (dired-mode . dired-hide-dotfiles-mode)
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "H" 'dired-hide-dotfiles-mode))
+
 ;; Dashboard
 ;; (require 'eliot-dashboard)
 
