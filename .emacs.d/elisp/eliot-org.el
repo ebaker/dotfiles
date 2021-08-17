20bfe0720804b23bcf0c65f7de3e81ca7bd72484
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-08-17 15:33:18 -0700

    (emacs) cleanup file indentation & disable org-mode auto indent

--- .emacs.d/elisp/eliot-org.el
+++ .emacs.d/elisp/eliot-org.el
@@ -3,26 +3,27 @@
 
 ;; @ebaker org-mode setyp
 
-(setq org-startup-indented t) ; Enable `org-indent-mode' by default
+(setq org-startup-indented nil) ; Enable `org-indent-mode' by default
+;; #+STARTUP: indent
 (add-hook 'org-mode-hook #'visual-line-mode)
 
 ;; org-directory
 (setq org-directory "~/org")
 
 (setq org-agenda-files (list
-			(concat org-directory "/todo.org")
-			(concat org-directory "/notes.org")
-			;; (concat org-directory "/yara.org")
-			(concat org-directory "/people.org")
-			(concat org-directory "/emacs.org")
-			(concat org-directory "/org.org")
-			(concat org-directory "/habits.org")
+      (concat org-directory "/todo.org")
+      (concat org-directory "/notes.org")
+      ;; (concat org-directory "/yara.org")
+      (concat org-directory "/people.org")
+      (concat org-directory "/emacs.org")
+      (concat org-directory "/org.org")
+      (concat org-directory "/habits.org")
 
-			;; @ebaker TODO - should capture files be hidden from day agenda view
-			(concat org-directory "/capture-emacs.org")
-			(concat org-directory "/capture-drafts.org")
-			(concat org-directory "/capture-beorg.org")
-			))
+      ;; @ebaker TODO - should capture files be hidden from day agenda view
+      (concat org-directory "/capture-emacs.org")
+      (concat org-directory "/capture-drafts.org")
+      (concat org-directory "/capture-beorg.org")
+      ))
 
 ;; @ebaker - agenda show five days starting yesterday - https://emacs.stackexchange.com/questions/12517/how-do-i-make-the-timespan-shown-by-org-agenda-start-yesterday
 (setq org-agenda-start-day "-1d")
@@ -34,9 +35,9 @@
 ;; TODO remove or fix
 (setq org-todo-keyword-faces
       '(("TODO" . (:foreground "#ff39a3" :weight bold))
-	("NEXT" . "#E35DBF")
-	("DONE" . (:foreground "#A2FF38" :weight bold))
-	))
+  ("NEXT" . "#E35DBF")
+  ("DONE" . (:foreground "#A2FF38" :weight bold))
+  ))
 
 
 ;; (custom-set-faces
@@ -56,7 +57,7 @@
 ;; (make-face 'eliot-done-face)
 ;; (set-face-foreground 'eliot-done-face "A2FF38")
 (font-lock-add-keywords nil
-			'(("TODO" . 'eliot-todo-face)))
+      '(("TODO" . 'eliot-todo-face)))
 ;; (font-lock-add-keywords 'org-mode
 ;;                         '(("DONE" . 'eliot-done-face)))
 
@@ -76,18 +77,18 @@
   ;; @ebaker - custom todo keywords
   ;; https://emacs.stackexchange.com/questions/31466/all-todos-how-to-set-different-colors-for-different-categories
   (setq org-todo-keywords
-	'(
-	  (sequence "TODO" "|" "DONE")
-	  ;; (sequence "TODO" "NEXT" "|" "DONE")
-	  ))
+  '(
+    (sequence "TODO" "|" "DONE")
+    ;; (sequence "TODO" "NEXT" "|" "DONE")
+    ))
 
   ;; @ebaker - TODO does this work or need debugging?
   ;; https://www.computerhope.com/cgi-bin/htmlcolor.pl?c=FF39A3
 
   (setq org-tag-faces
-	'(("TODO" . (:foreground "#ff39a3" :weight bold))
-	  ("DONE" . (:foreground "#A2FF38" :weight bold))
-	  ))
+  '(("TODO" . (:foreground "#ff39a3" :weight bold))
+    ("DONE" . (:foreground "#A2FF38" :weight bold))
+    ))
 
   ;; @ebaker - import org-archive-subtree-hierarchical
   ;; https://github.com/ebaker/el/org-archive-subtree-hierarchical.el
@@ -107,15 +108,15 @@
   ;; @ebaker - custom cature templates
   ;; TODO - source?
   (setq org-capture-templates
-	'(("n" "Notes" entry (file "~/org/capture-emacs.org")
-	   "* %^{Description} %^g %?
+  '(("n" "Notes" entry (file "~/org/capture-emacs.org")
+     "* %^{Description} %^g %?
 ded: %U")
-	  ("t" "Todo" entry (file "~/org/capture-emacs.org")
-	   "* TODO %^{Description} %^g %?
+    ("t" "Todo" entry (file "~/org/capture-emacs.org")
+     "* TODO %^{Description} %^g %?
 :PROPERTIES:
 :CREATED: %U
 :END:"))
-	)
+  )
 
   ;; @ebaker - org-refile
   ;; @ebaker - TODO source?
@@ -125,16 +126,16 @@ ded: %U")
   ;; (setq org-refile-targets (quote ((org-agenda-files :maxlevel . 2))))
 
   (setq org-refile-targets (quote (("~/org/todo.org" :maxlevel . 1)
-				   ("~/org/notes.org" :maxlevel . 2)
-				    ;; (concat org-directory "/yara.org" :maxlevel . 1)
-				   ("~/org/people.org" :maxlevel . 1)
-				   ("~/org/maybe.org" :maxlevel . 1)
+           ("~/org/notes.org" :maxlevel . 2)
+            ;; (concat org-directory "/yara.org" :maxlevel . 1)
+           ("~/org/people.org" :maxlevel . 1)
+           ("~/org/maybe.org" :maxlevel . 1)
 
-				   ("~/org/emacs.org" :maxlevel . 1)
-				   ("~/org/org.org" :maxlevel . 1)
-				   ("~/org/habits.org" :maxlevel . 1)
-				   ("~/org/computer.org" :maxlevel . 1)
-				   )))
+           ("~/org/emacs.org" :maxlevel . 1)
+           ("~/org/org.org" :maxlevel . 1)
+           ("~/org/habits.org" :maxlevel . 1)
+           ("~/org/computer.org" :maxlevel . 1)
+           )))
 
   (setq org-completion-use-ido nil)
 
@@ -146,11 +147,11 @@ ded: %U")
   ;; @ebaker agenda commands
   ;; - https://stackoverflow.com/questions/17182954/write-and-call-function-from-agenda-org-mode
   (setq org-agenda-custom-commands
-	'(("r" "[r]efile list for captured tasks/notes" tags "+refile")
-	  ;; match those tagged with :inbox:, are not scheduled, are not DONE.
-	  ("u" "[u]nscheduled tasks" tags "-refile-SCHEDULED={.+}/!-DONE")
-	  ("D" "[D]one tasks list" tags "+TODO=\"DONE\"-crypt")
-	)
+  '(("r" "[r]efile list for captured tasks/notes" tags "+refile")
+    ;; match those tagged with :inbox:, are not scheduled, are not DONE.
+    ("u" "[u]nscheduled tasks" tags "-refile-SCHEDULED={.+}/!-DONE")
+    ("D" "[D]one tasks list" tags "+TODO=\"DONE\"-crypt")
+  )
       )
 
   ;; @ebaker - TODO update colors
@@ -162,8 +163,8 @@ ded: %U")
 
   ;;set colours for priorities
   (setq org-priority-faces '((?A . (:foreground "#FF9538"))
-			     (?B . (:foreground "#FFB638"))
-			     (?C . (:foreground "#FFD738"))))
+           (?B . (:foreground "#FFB638"))
+           (?C . (:foreground "#FFD738"))))
 
 
   ;; TODO consider a refile & schedule function
