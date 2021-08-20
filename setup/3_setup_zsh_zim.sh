69855f82095229ac07c689674b3dff2a30062888
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-08-19 18:15:09 -0700

    (zsh) update 3_setup_zsh_zim script

--- setup/3_setup_zsh_zim.sh
+++ setup/3_setup_zsh_zim.sh
@@ -9,6 +9,7 @@ find $HOME -maxdepth 1 -name .zshrc.local -exec mv {} {}.bak \;
 find $HOME -maxdepth 1 -name .zlogin -exec mv {} {}.bak \;
 find $HOME -maxdepth 1 -name .zimrc -exec mv {} {}.bak \;
 find $HOME -maxdepth 1 -name .zim -exec mv {} {}.bak \;
+find $HOME -maxdepth 1 -name .zshenv -exec mv {} {}.bak \;
 
 # setup zsh & zim symlinks
 ln -vs $DOTFILES_DIR/.zsh $HOME/.zsh
@@ -17,3 +18,8 @@ ln -vs $DOTFILES_DIR/.zshrc.local $HOME/.zshrc.local
 ln -vs $DOTFILES_DIR/.zlogin $HOME/.zlogin
 ln -vs $DOTFILES_DIR/.zimrc $HOME/.zimrc
 ln -vs $DOTFILES_DIR/.zim $HOME/.zim
+ln -vs $DOTFILES_DIR/.zshenv $HOME/.zshenv
+
+# zimfw install
+curl -Lo $HOME/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+zsh $HOME/.zim/zimfw.zsh install
