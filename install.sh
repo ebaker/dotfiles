514ddf085d74183087f6d9f64fb61eb2c97525ac
Eliot Baker <430383+ebaker@users.noreply.github.com>
2022-04-19 00:48:52 -0400

    install: update script

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ DOTFILES_DIR=$(pwd)
 source $DOTFILES_DIR/setup/0_brew_install.sh
 source $DOTFILES_DIR/setup/1_setup_vim.sh
 # source $DOTFILES_DIR/setup/2_setup_nvm.sh
-source $DOTFILES_DIR/setup/3_setup_zsh_zim.sh
+# source $DOTFILES_DIR/setup/3_setup_zsh_zim.sh
+# source $DOTFILES_DIR/setup/4_setup_emacs.sh
