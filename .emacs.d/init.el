46da8717378b98e2ac37c02123d10a928b5fa2b5
Eliot Baker <430383+ebaker@users.noreply.github.com>
2023-07-19 21:54:55 -0700

    fixup! (emacs) update corfu config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -721,13 +721,15 @@ One for writing code and the other for reading articles."
   :custom
   (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
   (corfu-auto t)                 ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  (corfu-separator ?\s)          ;; Orderless field separator
   ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
   ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
   ;; (corfu-preview-current nil)    ;; Disable current candidate preview
   ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
   ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
   ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+  ;; (corfu-auto-delay 0.1)
+  (corfu-auto-prefix 1)
 
   ;; Enable Corfu only for certain modes.
   ;; :hook ((prog-mode . corfu-mode)
