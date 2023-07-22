224b72c48e37d2f271b2a85943554f4fbeed8614
Eliot Baker <430383+ebaker@users.noreply.github.com>
2023-07-21 22:45:46 -0700

    (emacs) add dired-sidebar-mode & vscode-icons

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -161,7 +161,8 @@
                  shell-mode-hook
                  eshell-mode-hook
                  vterm-mode-hook
-                 treemacs-mode-hook))
+                 treemacs-mode-hook
+                 dired-sidebar-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; emacs config hotkey
@@ -1711,6 +1712,28 @@ When using Homebrew, install it using \"brew install trash\"."
 ;; Elementary textual completion backend.
 ;; (setq company-backends
 ;; (add-to-list 'company-backends 'company-dabbrev))
+
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'vscode)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font nil))
+
+(use-package vscode-icon
+  :ensure t
+  :commands (vscode-icon-for-file))
 
 ;;; Terminal
 
